c521e8bbc110c39069616b79bcf9b16e5b93cb93
frizHub Mac1 <>
2020-03-10 18:40:50 +0500

    MVC Added

---
+++ Eventi-iOS/Eventi-iOS/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Eventi-iOS
+//
+//  Created by frizHub Mac1 on 10/03/2020.
+//  Copyright © 2020 frizHub Mac1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
