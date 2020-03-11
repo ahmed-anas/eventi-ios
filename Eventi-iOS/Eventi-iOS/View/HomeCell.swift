3798baab4f6a346b5439de8e86fb06d93a882785
frizHub Mac1 <>
2020-03-11 15:35:51 +0500

    Cells added

---
+++ Eventi-iOS/Eventi-iOS/View/HomeCell.swift
@@ -0,0 +1,25 @@
+//
+//  HomeCell.swift
+//  Eventi-iOS
+//
+//  Created by frizHub Mac1 on 11/03/2020.
+//  Copyright © 2020 frizHub Mac1. All rights reserved.
+//
+
+import UIKit
+
+class HomeCell: UICollectionViewCell {
+
+    @IBOutlet weak var cellImage: UIImageView!
+    @IBOutlet weak var cellLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    
+    
+
+}
