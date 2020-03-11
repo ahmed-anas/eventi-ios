0cefbb7e505b568bc9d37e249f6699de0a58b475
bazilhassan <>
2020-03-11 15:29:10 +0500

    program detail vc added

---
+++ Eventi-iOS/Eventi-iOS/Controller/VCProgramDetails.swift
@@ -0,0 +1,19 @@
+//
+//  VCProgramDetails.swift
+//  Eventi-iOS
+//
+//  Created by mac on 3/11/20.
+//  Copyright © 2020 frizHub Mac1. All rights reserved.
+//
+
+import UIKit
+
+class VCProgramDetails: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
